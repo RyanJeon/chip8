202937b1d7817958a9a06d5e59642d1c2b27e943
ryanjeon <yejeon2020@u.northwestern.edu>
2019-08-18 12:22:11 -0500

    Test_0 added

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-
+#include<assert.h>
 #include "../chip8.h"
 #include "../screen.h"
 
@@ -21,11 +21,21 @@ Chip8 initCPU(char path[]){
 }
 
 
-//0x0xxx test
+//MOVE ADD TEST 0x6xkk & 0x7xkk
 void TEST_0(){
 	
+	//Test_0 : 6122 7147
 	Chip8 chip = initCPU("Test_0");	
+	
+	//6122 : MOV v1, 0x22 <- 34 in decimal
 	chip.execute();
+	assert(chip.registerValue(1) == 0x22);
+
+	//7147 : ADD v1, 0x47 <- 71 in decimal 
+	chip.execute();
+	//total should be 105 in decimal, or 0x69
+	assert(chip.registerValue(1) == 0x69);
+
 }
 
 int main(int argc, char **argv){
